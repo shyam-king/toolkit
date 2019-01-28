e411f9ebcead62205e4d21fc70e8e38eadcc6f81
Shyam <m.shyam.tnj@gmail.com>
2019-01-28 15:49:00 +0530

    spider webdev added

---
+++ spider-webdev/submit.php
@@ -0,0 +1,24 @@
+<html>
+
+	<head>
+		<title> ILLUMINATI </title>
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+		<link rel="stylesheet" href="./styles.css" />
+	</head>
+	<body>
+		<div class="header">
+			<div style="float:left">
+			<img src="./illuminati.jpeg" width="64px" height="64px" />
+			</div>
+			<div style="float:left">
+				<p>ILLUMINATI</p>
+			</div>
+		</div>
+		</br>
+		<div class="content">
+		<h1> Welcome </h1>
+		<p><?php echo "Welcome to the darker world, " + $_POST['uname'] + "."; ?></p>
+		</div>
+	</body>
+</html>
